fdd019c62028b9ae66c9f107cabdba7b4fe77f08
Benjamin W. Broersma <bw@broersma.com>
2017-01-17 00:23:42 +0100

    Added Mozilla Readability for getting the text, is setting the innerHTML of a DOMElement a security issue?

--- src/activitylogger/content_script/index.js
+++ src/activitylogger/content_script/index.js
@@ -1,10 +1,30 @@
 import { getMetadata, metadataRules } from 'page-metadata-parser'
+import { Readability } from 'readability'
 
 // Get the basic info and text content (for search index) of this page.
 function gatherPageInfo() {
     // Extract info from all sorts of meta tags (og, twitter, etc.)
     const visitedUrl = window.location.href
-    const text = document.body.innerText
+
+    const location = document.location
+    const uri = {
+        spec: location.href,
+        host: location.host,
+        prePath: location.protocol + "//" + location.host,
+        scheme: location.protocol.substr(0, location.protocol.indexOf(":")),
+        pathBase: location.protocol + "//" + location.host + location.pathname.substr(0, location.pathname.lastIndexOf("/") + 1)
+    };
+    const documentClone = document.cloneNode(true)
+    const article = new Readability(uri, documentClone).parse()
+    let text;
+    if (article) {
+        const articleDom = document.createElement('main')
+        articleDom.innerHTML = article.content
+        text = articleDom.innerText
+    } else {
+        text = document.body.innerText
+    }
+
     const pageMetadata = getMetadata(document, visitedUrl, metadataRules)
     return {
         ...pageMetadata,
