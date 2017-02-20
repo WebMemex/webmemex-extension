583ca257dcbf5dacbdac3bd4be0894db3a0b7e66
Gerben <gerben@treora.com>
2017-02-20 18:41:33 +0100

    Ignore visits to same URL within last hour.

--- src/activity-logger/background/index.js
+++ src/activity-logger/background/index.js
@@ -2,7 +2,7 @@ import logPageVisit from './log-page-visit'
 import { isWorthRemembering } from '..'
 
 // Listen for navigations to log them and analyse the pages.
-browser.webNavigation.onCommitted.addListener(({url, tabId, frameId}) => {
+browser.webNavigation.onCompleted.addListener(({url, tabId, frameId}) => {
     // Ignore pages loaded in frames, it is usually noise.
     if (frameId !== 0)
         return
@@ -10,6 +10,39 @@ browser.webNavigation.onCommitted.addListener(({url, tabId, frameId}) => {
     if (!isWorthRemembering({url}))
         return
 
-    logPageVisit({url, tabId})
-
+    // Look if we just made a visit to this page...
+    checkIfExistingVisit({
+        url, // ...with this URL...
+        after: new Date()-1000*60*60, // ...less than an hour ago...
+        limit: 10, // ...and with less than ten pages visited in between.
+    }).then(visit => {
+        // If not just visited already, log this visit.
+        if (!visit) {
+            return logPageVisit({url, tabId})
+        }
+    })
 })
+
+import db from '../../pouchdb'
+import { convertVisitDocId, visitKeyPrefix } from '..'
+function checkIfExistingVisit({url, after, limit}) {
+    return db.allDocs({
+        startkey: `${visitKeyPrefix}\uffff`,
+        endkey: convertVisitDocId({timestamp: after}),
+        descending: true,
+        include_docs: true,
+        limit,
+    }).then(result => {
+        const matches = result.rows.filter(row =>
+            // Check if URL (up to fragment identifier) is the same
+            row.doc.url.split('#')[0] === url.split('#')[0]
+        )
+        if (matches.length) {
+            // Return the most recent match (presumably the only one)
+            return matches[matches.length-1]
+        }
+        else {
+            return undefined
+        }
+    })
+}
