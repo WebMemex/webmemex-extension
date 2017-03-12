99bbbbcb6aed76f6040d6a6af388a127a1888ba2
Nimmi Weeraddana <Nimmi Weeraddana>
2017-03-12 23:55:40 +0530

    45: modified the activity lo brackground script

--- src/activity-logger/background/index.js
+++ src/activity-logger/background/index.js
@@ -38,3 +38,68 @@ browser.webNavigation.onCommitted.addListener(details => {
         performPageAnalysis({pageId, tabId: details.tabId})
     })
 })
+
+
+//********************************************************************************/
+var timestamp = new Date().getTime;
+
+var number_of_opened_tabs = 0;
+
+var tabList = [];
+
+
+/*chrome.runtime.onMessage.addListener(function(response,sender,sendResponse){
+    alert(response);
+});*/
+
+
+browser.tabs.onCreated.addListener(function (tab){
+    number_of_opened_tabs = number_of_opened_tabs + 1;
+    //console.log('total opened tabs: ' + number_of_opened_tabs);
+    var t = {
+        id : tab.id,
+        activetime : 0,
+        isActive : false,
+        lastActivatedtime : Number(new Date().getTime())
+    };
+    tabList.push(t);
+    console.log('a new Tab created : ' + tab.id);
+})
+
+browser.tabs.onRemoved.addListener(function (id){
+    number_of_opened_tabs = number_of_opened_tabs - 1;
+    //console.log('total opened tabs: ' + number_of_opened_tabs);
+    for (var index in tabList){
+        if(tabList[index].id == id){
+            tabList.splice( index, 1 );
+        }
+    }
+    console.log('a Tab was closed : ' + id);
+})
+
+browser.tabs.onActivated.addListener(function (tab){
+    //console.log('Tab activated : ')
+    //console.log(tab);
+    for (var index in tabList){
+        if(tabList[index].isActive == true){
+            tabList[index].isActive = false;
+            tabList[index].lastActivatedtime = Number(new Date().getTime());
+            console.log('a tab went inactive : ' + tabList[index].id);
+        }
+        
+    }
+    for (var index in tabList){
+        if(tabList[index].id == tab.tabId){
+            var currTimestamp = Number(new Date().getTime())
+            var difference = currTimestamp - tabList[index].lastActivatedtime;
+            tabList[index].activetime = tabList[index].activetime + difference;
+            tabList[index].isActive = true;
+            console.log('a tab came active : ' + tabList[index].id);
+            console.log('tab ' + tabList[index].id +' had been in that page for '+ tabList[index].activetime + 'ms');
+        }
+        
+    }
+    
+})
+
+
