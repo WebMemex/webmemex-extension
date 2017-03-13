2446f490d5dd94ed570cec10957c311d6e920b82
Dwaipayan <dwaipayan2609@gmail.com>
2017-03-13 22:59:09 +0530

    Minor fixes

--- src/background.js
+++ src/background.js
@@ -1,5 +1,4 @@
 import './activity-logger/background'
-import importHistory from './browser-history/import-history'
 
 function openOverview() {
     browser.tabs.create({
@@ -9,9 +8,7 @@ function openOverview() {
 
 // Open the overview when the extension's button is clicked
 browser.browserAction.onClicked.addListener(() => {
-    console.log("Hello World");
-    //openOverview()
-    importHistory();
+    openOverview()
 })
 
 browser.commands.onCommand.addListener(command => {
