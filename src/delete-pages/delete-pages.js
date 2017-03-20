d6c45f94142e13753c64b157220190a967a3f1e1
Dwaipayan <dwaipayan2609@gmail.com>
2017-03-21 01:41:29 +0530

    Cleaned code

--- src/delete-pages/delete-pages.js
+++ src/delete-pages/delete-pages.js
@@ -1,29 +1,25 @@
 import db from '../pouchdb'
 import { findPagesByUrl, updatePageSearchIndex } from '../search/find-pages'
 
-const defaultResultLimit = 30
-
 //Delete all Page entries returned by findPagesByUrl
 function deletePageEntries(pagesResult) {
   console.log(pagesResult);
-  const pages = pagesResult.rows.map(row => ({
-    id: row.doc._id,
-    rev: row.doc_rev
-  }));
-  for (var key in pages) {
-    db.get(pages[key].id).then(doc => db.remove(doc._id, doc._rev)).then(() => {
-      console.timeEnd('Deleting pages end')
-      console.time('rebuild search index')
-      return updatePageSearchIndex()
-    }).then(() => {
-      console.timeEnd('rebuild search index')
-    }).catch(err => console.log(err));
-  }
+  const promises = pagesResult.rows.map(row => {
+    db.remove(row.doc).then(result => console.log(result))
+  })
+  return Promise.all(promises);
 }
 
 export default function deletePage(query) {
   console.log(query)
-  var result = findPagesByUrl({query, limit: defaultResultLimit}).then(
+  console.time('delete pages')
+  var result = findPagesByUrl({query}).then(
     deletePageEntries
-  )
+  ).then(() => {
+          console.timeEnd('delete pages')
+          console.time('rebuild search index')
+          return updatePageSearchIndex()
+      }).then(() => {
+          console.timeEnd('rebuild search index')
+      })
 }
