0dc70b79bfe8d0fe03ecc9d7392aa8652c160081
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-05-02 12:29:26 +0700

    Add support to inner-promises to promise-batcher
    - this changes the stream ops a bit
    - now the callback gets mapped to the stream that gets fed into the mergeMap; the mergeMap expects the callback to return a promise, hence it will operate on a stream of promises

--- src/util/promise-batcher.js
+++ src/util/promise-batcher.js
@@ -1,33 +1,46 @@
 import Rx from 'rxjs'
 
 // Given a RxJS Subject + Observable, makes that Observable pausable by sending bools to Subject
-const createPausable = (subject, getObserver) => subject.switchMap(running =>
-    Rx.Observable.if(() => running, getObserver(), Rx.Observable.empty()))
+const createPausable = (subject, getObservable) => subject.switchMap(running =>
+    Rx.Observable.if(() => running, getObservable(), Rx.Observable.empty()))
 
 /**
- * Given a Set of input and an async function, will attempt to concurrently batch fetch them in the background.
+ * Given a Set of input and an async function, will attempt to concurrently batch process them in the background.
+ * Note that if the async function resolves to another Promise that is wanted to be defered,
+ * handle resolving that in the `callback` and set `innerPromise`.
  *
  * @param {Iterable<any>} batch The total batch of input to operate on.
- * @param {(input: any) => Promise<any>} callback The async callback to run each input on.
+ * @param {(input: any) => Promise<any|Promise<any>>} callback The callback to run each input on, either
+ *      returning a promise or a promise that will return an inner promise to defer on.
+ * @param {boolean} [innerPromise=false] Denotes whether to defer on the Promise returned by callback,
+ *      or an inner Promise of that promise.
  * @param {number} [concurrency=5] How many promises to be waiting at any time.
  * @returns {any} Object containing batch handling functions.
  */
-function initBatch(batch, callback, concurrency = 5) {
+function initBatch({ batch, callback, innerPromise = false, concurrency = 5 }) {
     // State to keep track of progress
     const processed = new Set()
 
-    // Uses mergeMap to handle concurrently processing the deferred promises
-    const batchObservable = () => Rx.Observable.from(batch)
-        .filter(input => !processed.has(input)) // Ignore already-processed values
-        .mergeMap(
-            input => Rx.Observable.defer(() => callback(input)),
-            (input, result) => ({ input, result }),
-            concurrency,
-        )
-        .do(({ input }) => processed.add(input))  // Update state as input gets processed
+    const getObservable = () => {
+        const filteredInputObs = Rx.Observable.from(batch)
+            .filter(input => !processed.has(input))  // Ignore already-processed values
+
+        // Map input to pre-processing to emit promises to stream
+        // mergeMap needs to be used if innerPromise set (need to wait on outter promise)
+        const promiseObs = innerPromise ? filteredInputObs.mergeMap(callback) : filteredInputObs.map(callback)
+
+        // Main promise batching logic now that there is an observable stream of Promises
+        return promiseObs
+            .mergeMap(
+                promise => Rx.Observable.defer(promise),  // Defer all promises to stop them processing
+                (input, result) => ({ input, result }),
+                concurrency,  // Allow n promises to process at a time
+             )
+            .do(({ input }) => processed.add(input))  // Update state as input gets processed
+    }
 
     // Pauser subject to handle stopping, starting and resuming operations on input observable
-    const pauser = createPausable(new Rx.Subject(), batchObservable)
+    const pauser = createPausable(new Rx.Subject(), getObservable)
 
     // Interface to use this batch
     return {
@@ -45,4 +58,5 @@ function initBatch(batch, callback, concurrency = 5) {
 }
 
 // Export a HOF that binds callbacks to a batch initialser
-export default callback => (batch, concurrency) => initBatch(batch, callback, concurrency)
+export default (callback, innerPromise = false) =>
+    (batch, concurrency = 5) => initBatch({ batch, callback, innerPromise, concurrency })
