c8b1e5cd54565c58f8d69b61c770481e2f9da24e
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2017-05-08 18:56:04 +0700

    Update batcher to allow observer fns to be passed at init
    - start() function shouldn't really have to do that
    - makes anyone be able to init a batch and observe it some how while passing interface to someone else

--- src/util/promise-batcher.js
+++ src/util/promise-batcher.js
@@ -1,4 +1,6 @@
 import Rx from 'rxjs'
+import noop from 'lodash/noop'
+
 import State from './promise-batcher-state'
 
 /**
@@ -10,7 +12,12 @@ import State from './promise-batcher-state'
  * @param {number} [concurrency=5] How many promises to be waiting at any time.
  * @returns {any} Object containing batch handling functions.
  */
-function initBatch({ inputBatch, asyncCallback, concurrency = 5 }) {
+function initBatch({
+    inputBatch,
+    asyncCallback,
+    concurrency = 5,
+    observer = { next: noop, error: noop, complete: noop },
+}) {
     const state = new State() // State to keep track of progress
     let sub // State to keep track of subscription (allow hiding of Rx away from caller)
 
@@ -19,27 +26,24 @@ function initBatch({ inputBatch, asyncCallback, concurrency = 5 }) {
      * have been processed already, and filters new input against the state (to make
      * sure it isn't a dupe or if it was already processed on a previous invocation).
      */
-    const subBatchObservable = (...observer) => Rx.Observable.from(inputBatch)
+    const subBatchObservable = () => Rx.Observable.from(inputBatch)
         .filter(input => !state.has(input))  // Ignore already-processed values
         .mergeMap(
             input => Rx.Observable.defer(() => asyncCallback(input)), // Defer the async functions...
             (input, output) => ({ input, output }),
             concurrency)                                        // ...but run this many at any time
         .do(({ input }) => state.recordSuccess(input))  // Update state as input gets processed
-        .subscribe(...observer)
+        .subscribe(observer.next, observer.error, observer.complete)
 
     // Interface to use this batch
     return {
         /**
          * Starts/resumes the batched processing on the input.
-         * @param {({ input: any, output: any }) => void} onNext Callback to run after input is processed.
-         * @param {(err: any) => void} onError Callback to run for input if error gets thrown.
-         * @param {() => void} onCompleted Callback to run after ALL inputs are processed.
          * @returns {boolean} Denotes whether or not batch could be started/resumed.
          */
-        start(...observer) {
+        start() {
             if (sub) { return false }
-            sub = subBatchObservable(...observer) // Create new sub to an observable doing the processing
+            sub = subBatchObservable() // Create new sub to an observable doing the processing
             return true
         },
         /**
@@ -74,4 +78,5 @@ function initBatch({ inputBatch, asyncCallback, concurrency = 5 }) {
 
 // Export a function that binds async callbacks to a batch initialser
 export default asyncCallback =>
-    (inputBatch, concurrency = 5) => initBatch({ inputBatch, asyncCallback, concurrency })
+    (inputBatch, observer, concurrency = 5) =>
+        initBatch({ inputBatch, asyncCallback, concurrency, observer })
