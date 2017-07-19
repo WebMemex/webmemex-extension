11fac8e35e9dff43ba625a12f9dd48a22641e302
reficul31 <reficul31@gmail.com>
2017-07-19 11:06:56 +0530

    Unit tests for util module

---
+++ src/util/random-string.test.js
@@ -0,0 +1,10 @@
+/* eslint-env jest */
+
+import randomString from 'src/util/random-string'
+
+describe('randomString tests', () => {
+    test('should return a 10 digit random number', () => {
+        let expected = expect.stringMatching('^[0-9]{10}$')
+        expect(randomString()).toEqual(expected)
+    })
+})
