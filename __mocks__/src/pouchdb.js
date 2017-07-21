2e67ba98459251d1ec3571e4b9dcb69412f43af2
reficul31 <reficul31@gmail.com>
2017-07-21 11:12:47 +0530

    Setup for system tests

--- __mocks__/src/pouchdb.js
+++ __mocks__/src/pouchdb.js
@@ -1,11 +1,14 @@
+import fromPairs from 'lodash/fp/fromPairs'
 import PouchDB from 'pouchdb-core'
 
 import PouchDBMemory from 'pouchdb-adapter-memory'
+import PouchDBFind from 'pouchdb-find'
 import mapreduce from 'pouchdb-mapreduce'
 import replication from 'pouchdb-replication'
 
 PouchDB
   .plugin(PouchDBMemory)
+  .plugin(PouchDBFind)
   .plugin(mapreduce)
   .plugin(replication)
 
@@ -17,3 +20,22 @@ const pouchdbOptions = {
 
 const db = PouchDB(pouchdbOptions)
 export default db
+
+export const keyRangeForPrefix = prefix => ({
+    startkey: `${prefix}`,
+    endkey: `${prefix}\uffff`,
+})
+
+// Present db.find results in the same structure as other PouchDB results.
+export const normaliseFindResult = result => ({
+    rows: result.docs.map(doc => ({
+        doc,
+        id: doc._id,
+        key: doc._id,
+        value: {rev: doc._rev},
+    })),
+})
+
+// Get rows of a query result indexed by doc id, as an {id: row} object.
+export const resultRowsById = result =>
+    fromPairs(result.rows.map(row => [row.id, row]))
