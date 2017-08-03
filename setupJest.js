23e4815fec7047abcf5aa994afaa1d52bb47797e
reficul31 <reficul31@gmail.com>
2017-08-03 18:29:58 +0530

    Adding the refactored monkey patch

--- setupJest.js
+++ setupJest.js
@@ -1,49 +1,42 @@
 import iface from 'jsdom/lib/jsdom/living/generated/Element'
+import NODE_TYPE from 'jsdom/lib/jsdom/living/node-type'
 
 Element.prototype.insertAdjacentElement = function insertAdjacentElement(position, node) {
-    position = position.toLowerCase();
+    position = position.toLowerCase()
 
-    let context;
-    switch (position) {
-        case "beforebegin":
-        case "afterend": {
-            context = this.parentNode;
-            if (context === null || context.nodeType === NODE_TYPE.DOCUMENT_NODE) {
-              throw new DOMException(DOMException.NO_MODIFICATION_ALLOWED_ERR, "Cannot insert HTML adjacent to " +
-                "parent-less nodes or children of document nodes.");
-            }
-            break;
-        }
-        case "afterbegin":
-        case "beforeend": {
-            context = this;
-            break;
-        }
-        default: {
-            throw new DOMException(DOMException.SYNTAX_ERR, "Must provide one of \"beforebegin\", \"afterend\", " +"\"afterbegin\", or \"beforeend\".");
+    function checkHasValidParentNode(el) {
+        if (el.parentNode === null || el.parentNode.nodeType === NODE_TYPE.DOCUMENT_NODE) {
+            throw new DOMException(
+                DOMException.NO_MODIFICATION_ALLOWED_ERR,
+                "Cannot insert element adjacent to parent-less nodes or children of document nodes.",
+            )
         }
     }
 
-    // TODO: use context for parsing instead of a <template>.
-    const fragment = this.ownerDocument.createElement("template");
-    fragment.innerHTML = node.outerHTML;
-
     switch (position) {
         case "beforebegin": {
-            this.parentNode.insertBefore(fragment.content, this);
-            break;
-        }
-        case "afterbegin": {
-            this.insertBefore(fragment.content, this.firstChild);
-            break;
-        }
-        case "beforeend": {
-            this.appendChild(fragment.content);
-            break;
+            checkHasValidParentNode(this)
+            this.parentNode.insertBefore(node, this)
+            break
         }
         case "afterend": {
-            this.parentNode.insertBefore(fragment.content, this.nextSibling);
-            break;
+            checkHasValidParentNode(this)
+            this.parentNode.insertBefore(node, this.nextSibling)
+            break
+        }
+        case "afterbegin": {
+            this.insertBefore(node, this.firstChild)
+            break
+        }
+        case "beforeend": {
+            this.appendChild(node)
+            break
+        }
+        default: {
+            throw new DOMException(
+                DOMException.SYNTAX_ERR,
+                'Must provide one of "beforebegin", "afterend", "afterbegin", or "beforeend".',
+            )
         }
     }
 }
