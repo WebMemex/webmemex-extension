0d64b2af2208c063dc7933fa4ea9df49d614b3bc
Gerben <gerben@treora.com>
2017-09-18 00:11:53 +0200

    Expand tests for sync-location-hashes.

--- src/util/sync-location-hashes.test.js
+++ src/util/sync-location-hashes.test.js
@@ -2,27 +2,69 @@
 
 import syncLocationHashes from './sync-location-hashes'
 
-function createWindowStub({hash}) {
-    return [{
-        location: {hash},
-        addEventListener: jest.fn(),
-        removeEventListener: jest.fn(),
-    }]
-}
+
+const createWindowMock = () => ({
+    // FIXME When getting a (non-empty) hash attribute, location.hash should prepend a missing '#'.
+    // (therefore we now test the value of location.hash with a regex)
+    location: {hash: ''},
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+})
 
 describe('syncLocationHashes', () => {
-    test('should create a listener on the window', () => {
-        const windows = createWindowStub({hash: 'home'})
-        const disableListener = syncLocationHashes(windows, {initial: undefined})
-        expect(windows[0].addEventListener).toBeCalled()
-        disableListener()
+    const win1 = createWindowMock()
+    const win2 = createWindowMock()
+    const win3 = createWindowMock()
+    const windows = [win1, win2, win3]
+
+    beforeEach(() => {
+        windows.forEach(win => {
+            win.addEventListener.mockReset()
+            win.removeEventListener.mockReset()
+        })
+        win1.location.hash = '#win1hash'
+        win2.location.hash = '#win2hash'
+        win3.location.hash = '#win3hash'
     })
 
-    test('should disable the listener when return function is called', () => {
-        const windows = createWindowStub({hash: 'home'})
-        const disableListener = syncLocationHashes(windows, {initial: undefined})
-        disableListener()
-        expect(windows[0].removeEventListener).toBeCalled()
+    test('should create a listener on the windows', () => {
+        syncLocationHashes(windows, {initial: undefined})
+        windows.forEach(win => {
+            expect(win.addEventListener).toHaveBeenCalledTimes(1)
+        })
     })
-    // TODO test still requires to increase coverage over the code
+
+    test('should disable the listeners when returned function is called', () => {
+        const disableListener = syncLocationHashes(windows, {initial: undefined})
+        windows.forEach(win => {
+            expect(win.removeEventListener).not.toHaveBeenCalled()
+        })
+
+        disableListener()
+
+        windows.forEach(win => {
+            expect(win.removeEventListener.mock.calls).toEqual(win.addEventListener.mock.calls)
+        })
+    })
+
+    test('should directly perform an initial sync if specified', () => {
+        win2.location.hash = '#somehash'
+        syncLocationHashes(windows, {initial: win2})
+
+        windows.forEach(win => {
+            expect(win.location.hash).toMatch(/#?somehash/)
+        })
+    })
+
+    test('should sync to other windows when one emits a hashchange event', () => {
+        syncLocationHashes(windows, {initial: undefined})
+        const win2HashChangeEventListener = win2.addEventListener.mock.calls[0][1]
+
+        win2.location.hash = '#newhash'
+        win2HashChangeEventListener()
+
+        windows.forEach(win => {
+            expect(win.location.hash).toMatch(/#?newhash/)
+        })
+    })
 })
