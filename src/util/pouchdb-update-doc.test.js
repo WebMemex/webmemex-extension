125c820144aeee66ea3b03e17eca2ef2a47b7107
Gerben <gerben@treora.com>
2017-09-17 22:42:00 +0200

    Expand tests for pouchdb-update-doc.
    The two previous tests seemed to test the same thing, so removing one.
    Importing pouchdb seems unnecessary, just mocking put and get suffices.
    Adding tests for the case of update conflicts.

--- src/util/pouchdb-update-doc.test.js
+++ src/util/pouchdb-update-doc.test.js
@@ -1,29 +1,47 @@
 /* eslint-env jest */
 
 import updateDoc from './pouchdb-update-doc'
-import db from 'src/pouchdb'
 
 describe('updateDoc', () => {
-    test('should call the database functions', async () => {
-        db.put = jest.fn()
-        db.get = jest.fn()
-        const docId = 'page/1234567890123/1234567890'
-        const updateFunc = jest.fn()
-        await updateDoc(db, docId, updateFunc)
-        expect(db.get).toHaveBeenCalledWith(docId)
-        expect(db.put).toHaveBeenCalledTimes(1)
+    const db = {
+        get: jest.fn(),
+        put: jest.fn(),
+    }
+
+    beforeEach(() => {
+        db.get.mockReset()
+        db.put.mockReset()
     })
 
     test('should update the doc with the updateFunc', async () => {
         const docBeforeChange = {test: 1}
         const docAfterChange = {test: 2}
-        const docId = 'page/1234567890123/1234567890'
-        db.put = jest.fn()
-        db.get = jest.fn().mockReturnValueOnce(docBeforeChange)
+        const docId = 'mydoc'
+        db.get.mockReturnValueOnce(docBeforeChange)
         const updateFunc = jest.fn().mockReturnValueOnce(docAfterChange)
+
         await updateDoc(db, docId, updateFunc)
+
         expect(db.get).toHaveBeenCalledWith(docId)
         expect(updateFunc).toHaveBeenCalledWith(docBeforeChange)
         expect(db.put).toHaveBeenCalledWith(docAfterChange)
+    })
+
+    test('should survive a conflict', async () => {
+        const docId = 'mydoc'
+        const updateFunc = jest.fn()
+        const conflictError = {name: 'conflict', status: 409, error: true}
+        db.put.mockImplementationOnce(doc => { throw conflictError })
+
+        await expect(updateDoc(db, docId, updateFunc)).resolves.toBeUndefined()
+    })
+
+    test('should give up after persistent conflict', async () => {
+        const docId = 'mydoc'
+        const updateFunc = jest.fn()
+        const conflictError = {name: 'conflict', status: 409, error: true}
+        db.put.mockImplementation(doc => { throw conflictError })
+
+        await expect(updateDoc(db, docId, updateFunc)).rejects.toMatchObject(conflictError)
     })
 })
