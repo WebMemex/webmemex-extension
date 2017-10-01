e73aa40fb6dc6b330505823e664d10a3df7d339e
Gerben <gerben@treora.com>
2017-10-01 23:07:12 +0200

    Add some explanation to tree-walker source.

--- src/util/tree-walker.js
+++ src/util/tree-walker.js
@@ -1,6 +1,16 @@
-// Generic tree walking algorithms
+/* Generic tree walking algorithms
+ * In our context, a tree is defined by two things:
+ * 1. An object containing a pair of functions that let us walk from a given node to its neighbours:
+ *   walkFunctions = {
+ *     getParent :: node → node,
+ *     getChildren :: node → [node],
+ *   }
+ *   Either or both functions may be asynchronous.
+ * 2. A single node, to start walking from.
+ */
 
 // Given a node, return all nodes connected to it.
+// Example usage: const nodes = getAllNodes(walkFunctions)(node)
 export const getAllNodes = ({getParent, getChildren}) => async node => {
     // First find the root, then crawl down each branch.
     let root = await getRoot({getParent})(node)
@@ -19,6 +29,7 @@ export const getAllNodes = ({getParent, getChildren}) => async node => {
 }
 
 // Get the root (uppermost parent) of the tree of the given node.
+// Example usage: const root = await getRoot(walkFunctions)(node)
 export const getRoot = ({getParent}) => async node => {
     let root = node
     let next
