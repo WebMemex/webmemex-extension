2b15ac52419f6c6ccaf678b8fefad0f4f4de843e
Gerben <gerben@treora.com>
2018-09-12 17:40:01 +0200

    Remove call to encodeUriComponent.
    If only such encodings would work consistently everywhere..

--- src/local-page/index.js
+++ src/local-page/index.js
@@ -8,7 +8,7 @@ export function relativeUrlForLocalPage(page) {
         return undefined
     }
     const pageId = page._id
-    const url = `/local-page.html?page=${encodeURIComponent(pageId)}`
+    const url = `/local-page.html?page=${pageId}`
     const hash = (page.url && page.url.split('#')[1])
     const href = (hash !== undefined) ? url + '#' + hash : url
     return href
